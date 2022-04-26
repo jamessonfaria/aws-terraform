c5d3a20fff87675d35d037af7049dd36c6d0b66d
Jamesson Faria <jamesson.da.s.junior@accenture.com>
2022-04-26 12:23:01 -0300

    Using splat expressions

--- sample_foreach/outputs.tf
+++ sample_foreach/outputs.tf
@@ -1,15 +1,23 @@
 output "extensions" {
-    value = local.file_extensions
+  value = local.file_extensions
 }
 
 output "extensions_upper" {
-    value = local.file_extensions_upper
+  value = local.file_extensions_upper
 }
 
 output "instance_arns" {
-    value = [for k, v in aws_instance.this : v.arn]
+  value = [for k, v in aws_instance.this : v.arn]
 }
 
 output "instances_names" {
-    value = {for k, v in aws_instance.this : k => v.tags.Name}
+  value = { for k, v in aws_instance.this : k => v.tags.Name }
+}
+
+output "private_ips" {
+  value = [for o in local.ips : o.private]
+}
+
+output "public_ips" {
+  value = local.ips[*].public
 }
