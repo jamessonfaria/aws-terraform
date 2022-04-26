1b2cac33ec002a38639eabb91491c2b8d2170fff
Jamesson Faria <jamesson.da.s.junior@accenture.com>
2022-04-26 12:17:34 -0300

    Sample using if and others for

--- sample_foreach/locals.tf
+++ sample_foreach/locals.tf
@@ -4,4 +4,6 @@ locals {
 
     file_extensions = [for file in local.files : regex("\\.[0-9a-z]+$", file)]
 
+    file_extensions_upper = { for f in local.file_extensions : f => upper(f) if f != ".json" }
+
 }
--- sample_foreach/outputs.tf
+++ sample_foreach/outputs.tf
@@ -1,3 +1,15 @@
 output "extensions" {
     value = local.file_extensions
 }
+
+output "extensions_upper" {
+    value = local.file_extensions_upper
+}
+
+output "instance_arns" {
+    value = [for k, v in aws_instance.this : v.arn]
+}
+
+output "instances_names" {
+    value = {for k, v in aws_instance.this : k => v.tags.Name}
+}
